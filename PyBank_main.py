8c0abe67fc555903499551cc48be84016c9e78e5
Frank Garcia <frankagarcia@outlook.com>
2018-12-15 14:08:17 -0700

    Update PyBank_main.py
    I noticed I missed a line when I switched my code from VB Studio to Jupyter.  I have updated to include the missing code>  I apologize if this messed anything up on your end.  Thanks!

--- PyBank_main.py
+++ PyBank_main.py
@@ -9,6 +9,7 @@ def PyBank():
         csvheader = next(csvreader)
         total_month = 0
         total_profit = 0
+        starting_profit = 0
         monthly_profit = []
         monthly_increase = []
         profit = []
@@ -19,22 +20,31 @@ def PyBank():
             monthly_profit.append(row[1])
             #find total profit
             total_profit += int(row[1])
+            #find monthly profits
+            profit.append(int((row)[1]) - starting_profit)
+            starting_profit = int(row[1])
         #this is how I checked my data
         print("Financial Analysis")
         print("------------------")
         print(("Total Month: ") + str(total_month))
         print(("Total: $") + str(total_profit))
+        average_profit_change = total_profit / total_month
         print(("Average change: $") + str(round(average_profit_change, 2)))
-        print("Greatest increase in profits: $")
-        print("Greatest decrease in profits: $")
+        print("Greatest increase in profits: $ " + str(max(set(profit))))
+        print("Greatest decrease in profits: $ " + str(min(set(profit))))
 
-output = (
-    f"\nFinancial Analysis \n"
-    f"------------------\n"
-    f"Total Month: {total_month} \n"
-    f"Greatest increase in profits: $" + str(max(set(monthly_profit))) + "\n"
-    f"Greatest decrease in profits: $" + str(min(set(monthly_profit))))
-print(output)       
-
-with open(output_file, 'w', newline="") as datafile:
-    datafile.write(output)
+        output = (
+            f"\nFinancial Analysis \n"
+            f"------------------\n"
+            f"Total Month: {total_month} \n"
+            f"Total: $" + str(total_profit) + "\n"
+            f"Average change: $" + str(round(average_profit_change, 2)) + "\n"
+            f"Greatest increase in profits: $" + str(max(set(profit))) + "\n"
+            f"Greatest decrease in profits: $" + str(min(set(profit)))
+            )
+        print(output)      
+        
+        output_file = ("output_file.csv)
+        with open(output_file, 'w', newline="") as datafile:
+            datafile.write(output)
+PyBank()
